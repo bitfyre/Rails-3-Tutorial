044948ff62a711c59020ac85f29738480fbc90b9
Alex Lemanski <alex.lemanski@bitfyre.net>
2011-06-25 09:56:12 -0500

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,6 +22,11 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
 
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up", )
+  end
+
   # describe "GET /layout_links" do
   #   it "works! (now write some real specs)" do
   #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
