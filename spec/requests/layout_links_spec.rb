cc32cc7ef314094716f5304eb2879d86e84bd225
Alex Lemanski <alex.lemanski@bitfyre.net>
2011-07-09 23:38:27 -0500

    make sign up text consistent with book

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,6 +27,20 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign up", )
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+  end
+
   # describe "GET /layout_links" do
   #   it "works! (now write some real specs)" do
   #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
