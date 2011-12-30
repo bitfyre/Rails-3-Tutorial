1d8c5c9b8c6ae282c2fad1773c47baa180379bc2
Alex Lemanski <alex.lemanski@bitfyre.net>
2011-12-29 21:45:36 -0600

    add fields and attributes for passwords

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110921033320) do
+ActiveRecord::Schema.define(:version => 20111230033908) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,4 +93,14 @@ describe User do
       User.new(hash).should_not be_valid
     end
   end
+
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
