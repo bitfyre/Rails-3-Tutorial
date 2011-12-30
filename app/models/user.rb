5f0f92d7d47380ed4e7d10a9b49a6a5326f1826e
Alex Lemanski <alex.lemanski@bitfyre.net>
2011-12-29 22:43:37 -0600

    setup placeholders for password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,16 @@ class User < ActiveRecord::Base
   validates :password,  :presence => true,
                         :confirmation => true, 
                         :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  private
+    
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      string # Only a temporary implementation
+    end
 end
