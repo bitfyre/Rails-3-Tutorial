3a69f4208427201c686995f9adc02f35db46eb8e
Alex Lemanski <alex.lemanski@bitfyre.net>
2011-12-29 23:07:54 -0600

    add tests for matching and none mathching encrypted passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,12 @@ class User < ActiveRecord::Base
                         :length => { :within => 6..40 }
 
   before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+  end
 
   private
     
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,13 +98,23 @@ describe User do
     before(:each) do
       @user = User.create!(@attr)
     end
-    
+
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
 
     it "should set the encrypted password" do
       @user.encrypted_password.should_not be_blank
+    end
+
+    describe "has_password? method" do
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be flase if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
     end
   end
 end
